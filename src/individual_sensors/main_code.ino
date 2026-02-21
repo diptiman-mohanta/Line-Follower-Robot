0d4f69a8187f882f76cd36ded8e0fff93835e75f
Diptiman Mohanta <138050049+diptiman-mohanta@users.noreply.github.com>
2026-02-21 07:51:39 +0530

    V1: Adjust motor speed and PID constants but not tested yet
    Updated motor speed and PID constants for improved control. Added adaptive speed tracking based on sensor readings.

--- src/individual_sensors/main_code.ino
+++ src/individual_sensors/main_code.ino
@@ -12,52 +12,54 @@
 #define ir2  A3      // left sensor
 #define ir1  A4      //leftmost sensors
 
-int initial_motor_speed = 90;
+int initial_motor_speed = 100;
 int flag = 0;
 
 //PID CONSTANT
-float kp = 25;
-float ki = 0;
-float kd = 19;
+float kp = 28;
+float ki = 0.0;
+float kd = 22;
 
 float error = 0, P = 0, I = 0, D = 0, PID_value = 0;
 float previous_error = 0, previous_I = 0;
 
-
 int s1, s2, s3, s4, s5;
 
-const int lm1 = 75;
-const int lm2 = 75;
-const int rm1 = 92;
-const int rm2 = 92;
+const int lm1 = 80;
+const int lm2 = 80;
+const int rm1 = 95;
+const int rm2 = 95;
+
+// adaptive speed tracking
+int consecutive_straight = 0;
+
 void setup() {
-  // put your setup code here, to run once:
   pinMode(e1, OUTPUT);           //pwm pin 1
   pinMode(e2, OUTPUT);           //pwm pin 2
-  pinMode(i1, OUTPUT);           //ledt motor pin 1
+  pinMode(i1, OUTPUT);           //left motor pin 1
   pinMode(i2, OUTPUT);           //left motor pin 2
   pinMode(i3, OUTPUT);           //right motor pin 1
-  pinMode(i4, OUTPUT);           //rightmotor pin 2
+  pinMode(i4, OUTPUT);           //right motor pin 2
 
   pinMode(ir1 , INPUT);           //leftmost sensor
   pinMode(ir2 , INPUT);           //left sensor
   pinMode(ir3 , INPUT);           //middle sensor
   pinMode(ir4 , INPUT);           //right sensor
   pinMode(ir5 , INPUT);           //rightmost sensor
-  Serial.begin(9600);                      //setting serial monitor at a default baund rate of 9600
+
+  Serial.begin(9600);
   delay(1000);
-  //  Serial.println("Started !!");
-  //  delay(1000);
 }
+
 void readSensor()
 {
+  s1 = digitalRead(ir1);   //leftmost sensor
+  s2 = digitalRead(ir2);   // left sensor
+  s3 = digitalRead(ir3);   // mid sensor
+  s4 = digitalRead(ir4);   // right sensor
+  s5 = digitalRead(ir5);   // rightmost sensor
 
-  s1 = digitalRead(ir1);   //lftmst sensor
-  s2 = digitalRead(ir2);   // leftsensor
-  s3 = digitalRead(ir3);   // mid sen
-  s4 = digitalRead(ir4);   // right sen
-  s5 = digitalRead(ir5);    // rightmost sensor
-
+  // special case errors first (junction / lost line handling)
   if (s1 == 0 && s2 == 0 && s3 == 1 && s4 == 1 && s5 == 1)                   //turn right
   {
     error = 10;
@@ -66,19 +68,19 @@ void readSensor()
   {
     error = 11;
   }
-  else if (s1 == 1 && s2 == 1 && s3 == 1 && s4 == 1 && s5 == 1)              // Turn left side or stop
+  else if (s1 == 1 && s2 == 1 && s3 == 1 && s4 == 1 && s5 == 1)              // all sensors on line - go forward slightly
   {
     error = 12;
   }
-  else if (s1 == 0 && s2 == 0 && s3 == 0 && s4 == 0 && s5 == 0)             // make u turn
+  else if (s1 == 0 && s2 == 0 && s3 == 0 && s4 == 0 && s5 == 0)             // no sensor on line - go forward slightly
   {
     error = 13;
   }
-   else if (s1 == 1 && s2 == 0 && s3 == 0 && s4 == 0 && s5 == 1)             // make u turn
+  else if (s1 == 1 && s2 == 0 && s3 == 0 && s4 == 0 && s5 == 1)             // both outer sensors only
   {
     error = 14;
   }
-  
+
   // pid control sensors
   if (s1 == 0 && s2 == 0 && s3 == 0 && s4 == 0 && s5 == 1)
   {
@@ -118,8 +120,6 @@ void readSensor()
   }
 }
 
-
-//function to control the movements;
 void forward() {
   digitalWrite(i1, HIGH);
   digitalWrite(i2, LOW);
@@ -150,7 +150,6 @@ void sharpleft() {
   digitalWrite(i3, HIGH);
   digitalWrite(i4, LOW);
 }
-
 void right() {
   digitalWrite(i1, HIGH);
   digitalWrite(i2, LOW);
@@ -162,12 +161,13 @@ void sharpright() {
   digitalWrite(i2, LOW);
   digitalWrite(i3, LOW);
   digitalWrite(i4, HIGH);
-
 }
-void calculate_pid ()
+
+void calculate_pid()
 {
   P = error;
-  I = I + error;                                 //I = I+previous_I
+  I = I + error;
+  I = constrain(I, -50, 50);                    // anti-windup: clamp integral so it doesn't build up too much
   D = error - previous_error;
 
   PID_value = (kp * P) + (ki * I) + (kd * D);
@@ -176,74 +176,94 @@ void calculate_pid ()
   previous_error = error;
 }
 
-
 void motor_control()
 {
-  // Calculating the effective motor speed:
-  int left_motor_speed = initial_motor_speed + PID_value;
-  int right_motor_speed = initial_motor_speed - PID_value;
+  // adaptive base speed: go faster on straights, slower on curves
+  if (error == 0) {
+    consecutive_straight++;
+  } else {
+    consecutive_straight = 0;
+  }
 
-  // The motor speed should not exceed the max PWM value
-  left_motor_speed = constrain(left_motor_speed, 0, 255);
+  int adaptive_speed = initial_motor_speed;
+  if (consecutive_straight > 5) {
+    adaptive_speed = initial_motor_speed + 20;   // boost speed on long straights
+  }
+  if (abs(error) >= 3) {
+    adaptive_speed = initial_motor_speed - 15;   // slow down on sharp curves
+  }
+
+  adaptive_speed = constrain(adaptive_speed, 60, 220);
+
+  int left_motor_speed  = adaptive_speed + PID_value;
+  int right_motor_speed = adaptive_speed - PID_value;
+
+  left_motor_speed  = constrain(left_motor_speed,  0, 255);
   right_motor_speed = constrain(right_motor_speed, 0, 255);
 
+  analogWrite(e1, left_motor_speed + 20);   //Left Motor Speed (offset to compensate motor imbalance)
+  analogWrite(e2, right_motor_speed);        //Right Motor Speed
 
-  analogWrite(e1, left_motor_speed + 20); //Left Motor Speed
-  analogWrite(e2, right_motor_speed); //Right Motor Speed
-
-  //following lines of code are to make the bot move forward
   forward();
 }
+
 void loop()
 {
   readSensor();
-  if ( error == 10)                                               // Make right turn untill it detects straight path
-  { // calculate it by motor test
-//    analogWrite(e1, 90); //Left Motor Speed
-//    analogWrite(e2, 70); //Right Motor Speed
-//    forward();
-//    delay(105);
+
+  if (error == 10)                                               // Make right turn until it detects straight path
+  {
     do {
-      analogWrite(e1, rm1); //Left Motor Speed
-      analogWrite(e2, rm2); //Right Motor Speed
+      analogWrite(e1, rm1);
+      analogWrite(e2, rm2);
       right();
       readSensor();
     } while (s2 == 0 && s1 == 0 && error != -3);
+    previous_error = 0;                                          // reset PID state after sharp turn
+    I = 0;
   }
-  else if ( error == 11)                                               // Make left turn untill it detects straight path
+  else if (error == 11)                                          // Make left turn until it detects straight path
   {
-//    analogWrite(e1, 90); //Left Motor Speed
-//    analogWrite(e2, 70); //Right Motor Speed
-//    forward();
-//    delay(105);
     do {
-      //      readSensor();
-      analogWrite(e1, lm1); //Left Motor Speed
-      analogWrite(e2, lm2); //Right Motor Speed
+      analogWrite(e1, lm1);
+      analogWrite(e2, lm2);
       left();
       readSensor();
-    } while (s4 ==0 && s5 == 0 && error != 3);
+    } while (s4 == 0 && s5 == 0 && error != 3);
+    previous_error = 0;                                          // reset PID state after sharp turn
+    I = 0;
   }
   else if (error == 12)
   {
-        analogWrite(e1, 90); //Left Motor Speed
-    analogWrite(e2, 70); //Right Motor Speed
+    analogWrite(e1, 90);
+    analogWrite(e2, 70);
     forward();
     delay(20);
     readSensor();
   }
- else if (error == 13)
+  else if (error == 13)
   {
-        analogWrite(e1, 90); //Left Motor Speed
-    analogWrite(e2, 70); //Right Motor Speed
-    forward();
+    // use last known error direction to recover instead of blindly going forward
+    if (previous_error > 0) {
+      analogWrite(e1, rm1);
+      analogWrite(e2, rm2);
+      right();
+    } else if (previous_error < 0) {
+      analogWrite(e1, lm1);
+      analogWrite(e2, lm2);
+      left();
+    } else {
+      analogWrite(e1, 90);
+      analogWrite(e2, 70);
+      forward();
+    }
     delay(40);
     readSensor();
   }
-   else if (error == 14)
+  else if (error == 14)
   {
-        analogWrite(e1, 90); //Left Motor Speed
-    analogWrite(e2, 70); //Right Motor Speed
+    analogWrite(e1, 90);
+    analogWrite(e2, 70);
     forward();
     delay(60);
     readSensor();
